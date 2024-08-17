f0a81e3f8de058227ab974a89256d7373ed2283a
Zaher Abd Ulmoula <zaher88abd@gmail.com>
2024-08-17 16:35:10 -0400

    Show movie details in MovieDetails Fragment

--- app/src/main/java/dev/zaherabd/moviesapp/features/moviedetails/MovieDetailsFragment.kt
+++ app/src/main/java/dev/zaherabd/moviesapp/features/moviedetails/MovieDetailsFragment.kt
@@ -8,6 +8,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.NavArgs
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import dev.zaherabd.moviesapp.Constants
 import dev.zaherabd.moviesapp.Constants.TAG
 import dev.zaherabd.moviesapp.databinding.FragmentMoiveDetailsBinding
 import dev.zaherabd.moviesapp.network.module.MovieResponse
@@ -19,8 +22,6 @@ class MovieDetailsFragment : Fragment() {
 
     private var _binding: FragmentMoiveDetailsBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     private val args: MovieDetailsFragmentArgs by navArgs()
@@ -28,17 +29,21 @@ class MovieDetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentMoiveDetailsBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val movie = args.movieObj
-        Log.d(TAG, "onViewCreated: ${movie.id}")
-
+        binding.tvMovieTitle.text = movie.title
+        binding.tvMovieReleaseDate.text = movie.releaseDate
+        this.context?.let {
+            Glide.with(it)
+                .load(Constants.IMAGES_BASE_URL + movie.posterPath)
+                .diskCacheStrategy(DiskCacheStrategy.DATA)
+                .into(binding.ivMoviePoster)
+        }
     }
 
     override fun onDestroyView() {
