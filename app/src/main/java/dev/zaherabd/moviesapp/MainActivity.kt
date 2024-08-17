9e3346c816416d2be7ba04ccc65d002dc490e7c8
Zaher Abd Ulmoula <zaher88abd@gmail.com>
2024-08-16 20:20:58 -0400

    Add permission to connect to internet

--- app/src/main/java/dev/zaherabd/moviesapp/MainActivity.kt
+++ app/src/main/java/dev/zaherabd/moviesapp/MainActivity.kt
@@ -1,14 +1,13 @@
 package dev.zaherabd.moviesapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import dev.zaherabd.moviesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
