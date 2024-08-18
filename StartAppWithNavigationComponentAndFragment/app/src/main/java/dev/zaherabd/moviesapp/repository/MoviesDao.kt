ac13ab70b3774dbde3f40cf8b624bf4946c8dd2d
Zaher Abd Ulmoula <zaher88abd@gmail.com>
2024-08-18 13:07:14 -0400

    Add Room and Dao for movie

---
+++ StartAppWithNavigationComponentAndFragment/app/src/main/java/dev/zaherabd/moviesapp/repository/MoviesDao.kt
@@ -0,0 +1,17 @@
+package dev.zaherabd.moviesapp.repository
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface MoviesDao {
+    @Query("Select * from movies")
+    suspend fun getMovies():List<MovieEntity>
+    
+    @Insert
+    suspend fun insertMovie(movie:MovieEntity)
+
+    @Insert
+    suspend fun deleteMovie(movie: MovieEntity)
+}
